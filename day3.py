9c79029a08da012f0101b79b6a4c7d8e97b9f783
tjahjoe <ntell634@gmail.com>
2024-03-16 14:31:03 +0700

    soal 4

--- day3.py
+++ day3.py
@@ -20,3 +20,19 @@ for i in range(len(inpu)):
     else:
         inpu[i] = 0
 print(inpu)
+
+# 3
+inpu = ["A", "B", 'C']
+for indeks, i in enumerate(inpu):
+    print(f"{indeks} {i}")
+
+# 4
+inpu = [{"nama" : "Budi", "nilai" : 90}, {"nama" : "Dwi", "nilai" : 85}, {"nama" : "Tri", "nilai" : 75}]
+tertinggi = inpu[0]
+terrendah = inpu[0]
+for i in range(len(inpu)):
+    if tertinggi["nilai"] < inpu[i]["nilai"]:
+        tertinggi = inpu[i]
+    elif terrendah["nilai"] > inpu[i]["nilai"]:
+        terrendah = inpu[i]
+print(f"{tertinggi} \n {terrendah}")
