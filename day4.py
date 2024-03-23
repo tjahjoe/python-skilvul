9cbc7066e5225e5abb2cb1a2bbcd774cd5126d9b
tjahjoe <ntell634@gmail.com>
2024-03-23 15:22:37 +0700

    quiz 2

--- day4.py
+++ day4.py
@@ -13,4 +13,14 @@ angka = [2,3,10,12]
 angka = list(filter(lambda x : x % 2 == 0, angka))
 print(angka)
 
-# 
+# 3
+mirror =list(filter(lambda x : str(x) == str(x)[::-1], [9, 9]))
+
+def mi(a):
+    for i in range(len(a)//2):
+        if a[i] != a[i-1]:
+            return False
+        else :
+            return True
+
+print(mi(["a", "d", "a"]))
