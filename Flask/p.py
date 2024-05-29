6f9d33c36f04cde38619037749b6abecd812a604
tjahjoe <ntell634@gmail.com>
2024-05-29 21:26:24 +0700

    numpy

---
+++ Flask/p.py
@@ -0,0 +1,13 @@
+
+#import module 
+import numpy as np
+  
+# create an numpy array with 5 elements 
+data = np.array([1, 2, 3, 4, 5]) 
+print(data)
+
+a = np.array([1,2])
+b = np.array([1,2])
+b = np.vstack(b)
+
+print(a.dot(b))
